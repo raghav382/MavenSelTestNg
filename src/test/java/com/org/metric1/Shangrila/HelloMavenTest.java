591f0c5023db6cf4d0e347b0a46578b07eaada66
raghav382 <git config --global user.email raghavendraswamy122787@gmail.com>
2019-09-20 20:16:52 +0530

    Extent report migration

--- src/test/java/com/org/metric1/Shangrila/HelloMavenTest.java
+++ src/test/java/com/org/metric1/Shangrila/HelloMavenTest.java
@@ -11,7 +11,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HelloMavenTest{
 	
@@ -22,7 +24,21 @@ public class HelloMavenTest{
 	public void EntringTestFirst() throws InterruptedException
 	{
 		System.out.println("SecondTest");	
-		  Thread.sleep(5000);
+        driver.findElement(By.xpath("//a[text()=' REGISTER ']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("userName")));
+        driver.findElement(By.id("userName")).sendKeys("Raghavendra");
+        driver.findElement(By.id("usrPwd")).sendKeys("kavya382");
+        driver.findElement(By.id("cnfUsrPwd")).sendKeys("kavya382");
+        driver.findElement(By.xpath("//label[text()=\"Select Security Question\"]")).click();
+        driver.findElement(By.xpath("//span[text()='What is your pet name?']")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Security Answer']")).sendKeys("Raghav");
+        driver.findElement(By.xpath("//label[text()='Select Preferred Language']")).click();
+        driver.findElement(By.xpath("//span[text()='हिंदी']")).click();
+		JavascriptExecutor javae=(JavascriptExecutor)driver;
+		javae.executeScript("window.scrollBy(700,0)", "");
+		driver.navigate().back();
+		  Thread.sleep(2000);
 		  driver.close();
 	}
 	@Test(priority=1)
@@ -36,10 +52,8 @@ public class HelloMavenTest{
 		JavascriptExecutor javae=(JavascriptExecutor)driver;
 		javae.executeScript("window.scrollBy(0,700)", "");
 		Thread.sleep(3500);
-
-
-		ACT.sendKeys(Keys.ARROW_DOWN).perform();
-		ACT.sendKeys(Keys.ENTER).perform();
+		JavascriptExecutor java1e=(JavascriptExecutor)driver;
+		javae.executeScript("window.scrollBy(1200,0)", "");
 		
 	}
 
